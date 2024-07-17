2655b8ac29c834c54526e1240fb5dd51f239bc79
Raul <raulsmarius@yahoo.com>
2024-07-17 22:28:46 +0300

    Added a Sidebar

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 
 //const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const rootElem = document.querySelector('#root')
 const root = createRoot(rootElem)
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 )
---
+++ src/issues/IssueList.js
@@ -0,0 +1,5 @@
+const IssueList = () => {
+  return <h1>issue list</h1>
+}
+
+export default IssueList
